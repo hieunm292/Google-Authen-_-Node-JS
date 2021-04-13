457c824d4a897796776b8b8f74b357676fe10381
= <=>
2021-04-13 20:45:55 +0700

    authen

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const CLIENT_ID = '432168670314-omrcangnq2oft24kevcq5onso158i05k.apps.googleuser
 const client = new OAuth2Client(CLIENT_ID);
 
 app.set('view engine', 'ejs');
-
+app.use(express.static('public'));
 app.use(express.urlencoded(extended= true));
 app.use(express.json());
 
@@ -23,8 +23,8 @@ app.get('/login', (req, res) => {
     res.render('login')
 })
 
-app.post('/login'),(req,res)=>{
-    let token=req.nody.token;
+app.post('/login',(req,res)=>{
+    let token=req.body.token;
     console.log(token);
     async function verify() {
         const ticket = await client.verifyIdToken({
@@ -35,7 +35,50 @@ app.post('/login'),(req,res)=>{
         const userid = payload['sub'];
         console.log(payload)
     }
-    verify().catch(console.error);
+    verify()
+    .then(()=>{
+      res.cookie('session-id ', token)
+      res.send('successXX')
+    })
+    .catch(console.error);
+})
+
+app.get('/dashboard',checkAuthenticated ,(req,res) => {
+  let user=req.user;
+  res.render('dashboard',{user})
+})
+
+app.get('/protectedtoute',(req,res)=>{
+  res.render('protectedtoute')
+})
+
+app.get('/logout',(req,res)=>{
+  res.clearCookie('session-token')
+  res.redirect('login')
+})
+
+function checkAuthenticated(req, res, next){
+  let token = req.cookies['session-token'];
+  let user = {};
+  async function verify() {
+      const ticket = await client.verifyIdToken({
+          idToken: token,
+          audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      });
+      const payload = ticket.getPayload();
+      user.name = payload.name;
+      user.email = payload.email;
+      user.picture = payload.picture;
+    }
+    verify()
+    .then(()=>{
+        req.user = user;
+        next();
+    })
+    .catch(err=>{
+        res.redirect('/login')
+    })
+
 }
 
 app.listen(port, () => {
